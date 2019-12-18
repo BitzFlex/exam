5456b872beb1915ae5dfcde4eaa08c30fc9444e9
bhseong <bhseong@rose.local>
2019-12-18 13:39:31 +0900

    init

---
+++ lib/exam.ex
@@ -0,0 +1,18 @@
+defmodule Exam do
+  @moduledoc """
+  Documentation for Exam.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exam.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/lotto.ex
@@ -0,0 +1,15 @@
+defmodule Lotto do
+    @moduledoc """
+    """
+
+    @doc false
+    def lotto do
+        Enum.to_list(1..45)
+            |> Enum.shuffle
+            |> Enum.slice(0..5)
+            |> Enum.sort
+            |> Enum.join(",")
+            |> IO.puts
+    end
+
+  end
